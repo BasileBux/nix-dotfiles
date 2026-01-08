640a80deb3823d26760e62581110a0bc452d9a1d
BasileBux <basile.buxtorf@ik.me>
2026-01-08 08:42:45 +0100

    last fix to android-studio shortcut

--- shells/android/flake.nix
+++ shells/android/flake.nix
@@ -27,8 +27,6 @@
             pkgs.jdk17
             pkgs.gradle
             pkgs.git
-            (pkgs.writeShellScriptBin "astu"
-              "android-studio $1 > /dev/null 2>&1 &")
 
             # Emulator ran with the `run-test-emulator` command
             (pkgs.androidenv.emulateApp {
@@ -42,15 +40,15 @@
             })
 
             (pkgs.writeShellScriptBin "emu"
-              "	run-test-emulator > /dev/null 2>&1 &\n")
+              "run-test-emulator > /dev/null 2>&1 &\n")
             (pkgs.writeShellScriptBin "astudio"
-              "	android-studio  > /dev/null 2>&1 &\n")
+              "android-studio $1 > /dev/null 2>&1 &\n")
             (pkgs.writeShellScriptBin "build"
-              "	$1/gradlew :app:assembleDebug\n")
+              "$1/gradlew :app:assembleDebug\n")
             (pkgs.writeShellScriptBin "apkinstall"
-              "	adb install -r ${apkPath}\n")
+              "adb install -r ${apkPath}\n")
             (pkgs.writeShellScriptBin "reload"
-              "	adb shell input keyevent 4\n	./gradlew :app:assembleDebug\n	adb install -r ${apkPath}\n	adb shell am start -n ${packageName}/${packageName}.${activityName}\n")
+              "adb shell input keyevent 4\n	./gradlew :app:assembleDebug\n	adb install -r ${apkPath}\n	adb shell am start -n ${packageName}/${packageName}.${activityName}\n")
           ];
           shellHook =
             "	export ANDROID_HOME=\"$HOME/Android\"\n	export ANDROID_SDK_ROOT=\"$ANDROID_HOME\"\n	export PATH=\"$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator:$PATH:$ANDROID_HOME/tools/bin\"\n	export JAVA_HOME=\"${pkgs.jdk17.home}\"\n	export QT_QPA_PLATFORM=xcb\n\n	echo \"Android dev shell ready.\"\n	echo \"Package: ${packageName}\"\n	echo \"Activity: ${activityName}\"\n	echo \"APK Path: ${apkPath}\"\n	exec zsh\n";
