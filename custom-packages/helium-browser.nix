b00b1bbea14a269c0f2f009fdeee7100e762d7e4
BasileBux <basile.buxtorf@ik.me>
2026-01-19 13:51:42 +0100

    bumped helium version

--- custom-packages/helium-browser.nix
+++ custom-packages/helium-browser.nix
@@ -2,14 +2,14 @@
 
 let
   mkAppImage = import ./mkAppImage.nix { inherit pkgs; };
-  version = "0.8.1.1";
+  version = "0.8.2.1";
   pname = "helium";
 in mkAppImage {
   pname = pname;
   version = version;
   url =
     "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-  hash = "sha256-n1wn80h9O7GpZz4AygNSKMcilX8lr6fJkiQBBPPQXok=";
+  hash = "sha256-abNHp4WaR2MqidG42TErJZBR1PcUVtRpBYFiKCmadW8=";
 }
 # NOTE: To get the latest version (which should be updated regularly), run:
 # curl -sSf https://api.github.com/repos/imputnet/helium-linux/releases/latest | jq -r '.tag_name'
