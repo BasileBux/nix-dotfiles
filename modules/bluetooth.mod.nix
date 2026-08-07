2470151dab18127345b81f3ea291677531ccc91a
BasileBux <basile.buxtorf@ik.me>
2026-08-07 14:59:30 +0200

    bluetooth broken driver fix

--- modules/bluetooth.mod.nix
+++ modules/bluetooth.mod.nix
@@ -3,6 +3,19 @@
   flake.nixosModules.bluetooth = { pkgs, ... }: {
     hardware.bluetooth.enable = true;
     services.blueman.enable = true;
+
+    # Fix MediaTek MT7921 firmware bug: missing completion reports
+    # cause A2DP transport disconnection. Disabling USB autosuspend
+    # for the BT adapter prevents the chip from going into low-power
+    # states that trigger the firmware bug.
+    boot.kernelParams = [ "btusb.enable_autosuspend=n" ];
+
+    # Also disable USB autosuspend via udev for the MT7921 BT interface
+    services.udev.extraRules = ''
+      # MediaTek MT7921 Bluetooth: disable autosuspend to prevent A2DP dropouts
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0e8d", ATTR{idProduct}=="*", TEST=="power/control", ATTR{power/control}="on"
+    '';
+
     environment.systemPackages = with pkgs; [
       blueman
     ];
