52aeedc51c630c33bfdadcf5530cc47db2e8b305
BasileBux <basile.buxtorf@ik.me>
2026-08-16 15:09:59 +0200

    Revert parts of "kamina: add remote power-off service account for upsnap"

--- hosts/kamina/extra-config.nix
+++ hosts/kamina/extra-config.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }:
+{ pkgs, ... }:
 {
   boot = {
     loader = {
@@ -17,30 +17,4 @@
   };
 
   services.power-profiles-daemon.enable = true;
-
-  users.groups.remote-poweroff = { };
-
-  users.users."upsnap-poweroff" = {
-    isSystemUser = true;
-    group = "remote-poweroff";
-    extraGroups = [ "remote-poweroff" ];
-    shell = pkgs.bash;
-    openssh.authorizedKeys.keys = [
-      ''command="${pkgs.systemd}/bin/systemctl poweroff",no-agent-forwarding,no-port-forwarding,no-X11-forwarding,no-pty,no-user-rc ${inputs.self.serviceKeys.upsnap}''
-    ];
-  };
-
-  security.polkit.extraConfig = ''
-    polkit.addRule(function(action, subject) {
-      var allowedActions = [
-        "org.freedesktop.login1.power-off",
-        "org.freedesktop.login1.power-off-multiple-sessions"
-      ];
-
-      if (subject.isInGroup("remote-poweroff") &&
-          allowedActions.indexOf(action.id) !== -1) {
-        return polkit.Result.YES;
-      }
-    });
-  '';
 }
--- hosts/kamina/kamina.mod.nix
+++ hosts/kamina/kamina.mod.nix
@@ -16,7 +16,6 @@
       smb
       tailscale
       slop
-      polkit
     ];
     nixosModules = [
       ./extra-config.nix
