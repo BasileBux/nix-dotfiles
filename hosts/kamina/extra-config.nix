4e9b74a6acddd73361b6ec332c01c586db8817ae
BasileBux <basile.buxtorf@ik.me>
2026-08-16 17:29:32 +0200

    Prepared for upsnap service on kamina host (receiving from upsnap)

--- hosts/kamina/extra-config.nix
+++ hosts/kamina/extra-config.nix
@@ -13,8 +13,20 @@
     interfaces.enp3s0.wakeOnLan.enable = true;
     firewall = {
       allowedUDPPorts = [ 9 ];
+      allowPing = true;
     };
   };
+
+  # Used for upsnap WOL + remote shutdown
+  security.sudo.extraRules = [
+    {
+      users = [ "basileb" ];
+      commands = [
+        { command = "/run/current-system/sw/bin/poweroff"; options = [ "NOPASSWD" ]; }
+        { command = "/run/current-system/sw/bin/reboot";   options = [ "NOPASSWD" ]; }
+      ];
+    }
+  ];
 
   services.power-profiles-daemon.enable = true;
 }
--- hosts/kamina/kamina.mod.nix
+++ hosts/kamina/kamina.mod.nix
@@ -22,7 +22,10 @@
       {
         my.nushell.accentColor = "#fbe61e"; # Other possible options: #1eebfb #8b1efb #1efb8b
         my.ssh.extraAuthorizedKeys = [
+          # ipad / iphone through tailscale (termius)
           "from=\"100.122.103.5,100.84.142.57\"  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3JtMPAdhCamdxtzssUutjNaCyGtsUVltvtLakwuPze"
+          # upsnap on the raspberry pi through tailscale and eth0/wlan0 LAN
+          "from=\"192.168.1.191,192.168.1.190,100.80.171.121\"  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDUZ/0p/7j6gcvZS1Cm8f2PP2dzrNN5WSxvMJQaPLzFv upsnap@raspberrypi"
         ];
       }
     ];
